9071627600ebd43736a6c096928a4b6d48cd89da
Peter Bryzgalov <peterbryz@yahoo.com>
2017-02-24 20:17:59 +0900

    Enable RabbitMQ management plugin (can see broker in Flower broker tab).

--- start_celery_master.sh
+++ start_celery_master.sh
@@ -11,6 +11,9 @@
 # Start RabbitMq and Celery master containers
 #echo "Starting Rabbit"
 docker run -d -p 25672:25672 -p 4369:4369 -p 5672:5672 --name $rabbit_cont_name rabbitmq:3
+sleep 1
+docker exec -t rabbit rabbitmq-plugins enable rabbitmq_management && rabbitmqctl stop
+docker start rabbit
 sleep 2
 #echo "Starting Celery master with Flower"
 docker run -d --link $rabbit_cont_name:rabbit -v "$(pwd)":/root --name $celery_cont_name -p 5555:5555 $master_image
