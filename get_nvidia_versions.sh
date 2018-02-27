86c1f66093b95a30b275eff6d9f21784d768a907
Peter Bryzgalov <peterbryz@yahoo.com>
2018-02-27 11:20:38 +0900

    Finished v1 script for getting CUDA versions.

--- get_nvidia_versions.sh
+++ get_nvidia_versions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Test installed NVIDIA drivers, CUDA and cuDNN versions on Ubuntu
+# Ver.1.0
+# 2018 (C) Peter Bryzgalov @ CHITECH Stair Lab
 
 # Get NVIDIA driver version
 version_file="/proc/driver/nvidia/version"
@@ -18,6 +20,61 @@ if [ ! -f "$version_file" ]; then
 	return_states
 fi
 
+# Files should exist and permissions should be all 666
+if [ "$(stat -c "%a" /dev/nvidia* | grep -v 666)" ]; then
+	echo "Device files /dev/nvidia* permissions should be all 666"
+	stat -c "%a %n" /dev/nvidia*
+fi
+
+# Driver version
 NV_DRV=$(grep -Eo "Kernel Module\s+([0-9\.]+)" /proc/driver/nvidia/version | awk '{ print $3 }')
 #echo $NV_DRV
+
+
+function CheckCudaLocation {
+	CUDA_LOCATION=$1; shift
+	# If path is a link
+	if [ -h "$CUDA_LOCATION" ];then
+		# Check that readlink command exists
+		if [ ! "$(readlink /  2>&1 1>/dev/null)" ]; then
+			# Get link target
+			CUDA=$(readlink $CUDA_LOCATION | grep -Eo "[0-9\.]+")
+		else
+			# readlink command not present
+			CUDA=$(file /usr/local/cuda | grep -Eo "[0-9\.]+")
+		fi
+	elif [[ -d "$CUDA_LOCATION" ]]; then
+		CUDA=$(echo $CUDA_LOCATION | grep -Eo "[0-9\.]+")
+	else
+		CUDA=""
+	fi
+	echo $CUDA
+}
+
+# CUDA in path
+CUDA_IN_PATH=$(echo $PATH | grep -Eoi "[a-zA-Z\/\.\-\_]*cuda[a-z0-9\.\-]*")
+if [ "$CUDA_IN_PATH" ]; then
+	CUDA=$(CheckCudaLocation "$CUDA_IN_PATH")
+	if [ ! "$CUDA" ]; then
+		echo "CUDA path in PATH envvar does not exist."
+		CUDA="x"
+	fi
+else
+	# Try default location
+	LOCATION="/usr/local/cuda"
+	CUDA=$(CheckCudaLocation "$LOCATION")
+	if [ ! "$CUDA" ]; then
+		echo "CUDA not found on this system"
+		CUDA="x"
+	fi
+fi
+
+# Get cuDNN version
+CUDNN_PKG=$(dpkg --get-selections | grep -Eo "libcudnn[0-9]\s")
+if [ ! "$CUDNN_PKG" ]; then
+	echo "cuDNN not installed"
+else
+	cuDNN=$(dpkg -s $CUDNN_PKG | grep -i "version:" | grep -Eo "[0-9\.]+-[0-9]+")
+fi
+
 return_states
