5f382f6d8dc4e8195c524e676fcd8bce96e945f1
Peter Bryzgalov <peterbryz@yahoo.com>
2018-03-09 19:19:11 +0900

    Toy multiprocessing sample "multiprocessing tests" works: can read output from queue, cat get results. mlframe_dev_1 can send stdout and stderr through queues, but first line to stdout is always lost (using dummy line). Exit code looks good.

--- mlframe/scripts/test.sh
+++ mlframe/scripts/test.sh
@@ -4,8 +4,9 @@ if [ $1 ]; then
 	N=$1
 fi
 echo "Running test command on $(hostname)"
-for i in {1..6}; do
+for i in {1..10}; do
 	echo "$(hostname)($N):$i"
-	sleep 1
+	sleep 0.1
 	echo "$(hostname) err" >&2
 done
+exit 125
