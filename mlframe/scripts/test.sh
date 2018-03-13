2779eba3d40aa5175efb8b590d7c5c77973c79ba
Peter Bryzgalov <peterbryz@yahoo.com>
2018-03-13 18:45:09 +0900

    In mlframe_OO_dev_2 can: execute commands on localhost in background, get executed command output and exitcode later. It works with BashExecutor. With Host class can: ping host, test connection and execute command on the host, store its commands history with output and exitcodes.

--- mlframe/scripts/test.sh
+++ mlframe/scripts/test.sh
@@ -4,9 +4,10 @@ if [ $1 ]; then
 	N=$1
 fi
 echo "Running test command on $(hostname)"
-for i in {1..2}; do
+for i in {1..5}; do
 	echo "$(hostname)($N):$i"
-	sleep 0.4
+	echo "output $i"
+	#sleep 0.4
 	echo "$(hostname) err$i" >&2
 done
 exit 125
