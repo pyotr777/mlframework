00702f2100601bbe40d5a1082acf01c310463b12
Peter Bryzgalov <peterbryz@yahoo.com>
2018-03-14 20:15:29 +0900

    End-day commit.

--- mlframe/scripts/test.sh
+++ mlframe/scripts/test.sh
@@ -3,11 +3,11 @@
 if [ $1 ]; then
 	N=$1
 fi
-echo "Running test command on $(hostname)"
+echo "Running test command on $(hostname) pars: $@"
 for i in {1..5}; do
 	echo "$(hostname)($N):$i"
-	echo "output $i"
-	#sleep 0.4
-	echo "$(hostname) err$i" >&2
+	echo "($N):$i"
+	sleep 1
+	echo "$(hostname)($N) err:$i" >&2
 done
 exit 125
